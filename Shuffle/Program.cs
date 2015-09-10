ed64d85e2193f2348e6558e94ea99e6d73c0204f
Horia Toma <toma.horia@gmail.com>
2015-09-11 00:11:36 +0200

    Added random from unknown size collection

---
+++ Shuffle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shuffle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Helper.GetRandomFromOnlineData(new []{3, 4, 5, 6, 7, 8}));
+        }
+    }
+}
