9d11f08f552e075161d2340f2911d8f674b2f6b4
Horia Toma <toma.horia@gmail.com>
2015-09-11 22:53:16 +0200

    Added method for selecting random subset of size k from online data

---
+++ Shuffle/HelperRandomData.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shuffle
+{
+    public static class HelperRandomData
+    {
+        /// <summary>
+        /// select randomly an element from a collection whose size is unknown
+        /// at step i, the probability of selecting element is 1/i
+        /// </summary>
+       public static T GetRandomElement<T>(IEnumerable<T> data)
+        {
+            //note: you can pass random as an argument
+            var random = new Random();
+            var result = default(T);
+            int step = 0;
+            foreach (var el in data)
+            {
+                step++;
+                if (random.Next(step) == 0)
+                {
+                    result = el;
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// select randomly a subset of size k from online data
+        /// </summary>
+        /// <param name="k">size of subset</param>
+        public static IEnumerable<T> GetRandomSubset<T>(IEnumerable<T> data, int k)
+        {
+            // note: you can pass random as an argument
+            var random = new Random();
+            var result = new List<T>();
+            int step = 0;
+            foreach (var el in data)
+            {
+                step++;
+                if (step <= k)
+                {
+                    // initially store in the result the first k elements in the collection
+                    result.Add(el);
+                    continue;
+                }
+
+                // at step i, probability to be selected is k/i
+                int pos = random.Next(step);
+                if (pos < k)
+                {
+                    // store element in the result at random position
+                    result[pos] = el;
+                }
+            }
+            return result;
+        }
+    }
+}
--- Shuffle/Program.cs
+++ Shuffle/Program.cs
@@ -6,7 +6,11 @@ namespace Shuffle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Helper.GetRandomFromOnlineData(new []{3, 4, 5, 6, 7, 8}));
+            //Console.WriteLine(HelperRandomData.GetRandomElement(new []{3, 4, 5, 6, 7, 8}));
+            foreach (var el in HelperRandomData.GetRandomSubset(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 3))
+            {
+                Console.WriteLine(el);
+            }
         }
     }
 }
